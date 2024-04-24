21aa0c2eb868ad9085ca325a5b189c88622c9006
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2024-04-25 00:09:02 +0300

    wip: audio worklets

--- src/karplus/karplus-strong-processor.ts
+++ src/karplus/karplus-strong-processor.ts
@@ -3,55 +3,48 @@ class KarplusStrongProcessor extends AudioWorkletProcessor {
     static get parameterDescriptors() {
         return [
             { name: 'damping', defaultValue: 0.5, minValue: 0, maxValue: 1 },
-            { name: 'frequency', defaultValue: 440, minValue: 20, maxValue: 5000 }
+            { name: 'frequency', defaultValue: 440, minValue: 20, maxValue: 5000 },
+            { name: 'sampleRate', defaultValue: 44100 }
         ];
     }
 
 
     n: number;
+    b: number;
     buffer: Float32Array | null;
     constructor() {
         super();
 
         this.buffer = null;
         this.n = 0;
+        this.b = 0;
 
     }
     process(inputs: Float32Array[][], outputs: Float32Array[][], parameters: Record<string, Float32Array>) {
         const frequency = parameters.frequency.length > 0 ? parameters.frequency[0] : 440;
+        const sampleRate = parameters.sampleRate.length > 0 ? parameters.sampleRate[0] : 44100;
 
 
         if (!this.buffer) {
-            const bufferSize = Math.round(44100 / frequency);
-            this.buffer = this.createBuffer(bufferSize);
+            const bufferSize = Math.round(sampleRate / frequency);
+            this.buffer = new Float32Array(bufferSize);
         }
 
         const N = this.buffer.length;
         const gain = 0.995;
         const output = outputs[0][0];
-        const impulse = Math.round(0.0001 * 44100);
         for (let i = 0; i < output.length; i++) {
-            const sample = gain * (this.buffer[this.n] + this.buffer[(this.n + 1) % N]) / 2;
+            const noise = this.b < N ? Math.random() * 2 - 1 : 0;
+            const sample = gain * (noise + (this.buffer[this.n] + this.buffer[(this.n + 1) % N])) / 2;
 
-            output[i] = this.buffer[this.n];
+            output[i] = sample;
 
             this.buffer[this.n] = sample;
             this.n = (this.n + 1) % N
+            this.b++;
         }
 
         return true;
-    }
-
-    createBuffer(size: number = 2048) {
-        const buffer = new Float32Array(size);
-
-        const impulse = Math.round(0.0001 * 44100);
-        // Initialize the buffer with random noise
-        for (let i = 0; i < size; i++) {
-            buffer[i] = i < impulse ? Math.random() * 2 - 1 : 0;
-        }
-
-        return buffer;
     }
 }
 
